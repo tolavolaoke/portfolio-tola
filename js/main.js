5f057258937bb1c7a73b02eadfdad52503f6b0d1
tolavolaoke <tolavolaoke@hotmail.co.uk>
2018-01-01 19:27:13 +0000

    added soome styling and one project image

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 window.onload = function(){ 
+
     // Get the modal
  var modal = document.getElementById('myModal');
  
  // Get the button that opens the modal
  var btn = document.getElementById("myBtn");
- 
+
  // Get the <span> element that closes the modal
  var span = document.getElementsByClassName("close")[0];
  
  // When the user clicks the button, open the modal 
- btns.onclick = function() {
+ btn.onclick = function() {
      modal.style.display = "block";
- }
- 
+    }
+
  // When the user clicks on <span> (x), close the modal
  span.onclick = function() {
      modal.style.display = "none";
@@ -23,7 +24,8 @@ window.onload = function(){
      if (event.target == modal) {
          modal.style.display = "none";
      }
- }
+ }  
  };
- 
+
+
 
