4ec79fc2443392ac12fcf3ddf8dc9f1aefe516fd
tolavolaoke <tolavolaoke@hotmail.co.uk>
2018-01-02 17:12:16 +0000

    fixed all modal buttons for projects

--- js/main.js
+++ js/main.js
@@ -5,9 +5,13 @@ window.onload = function(){
  
  // Get the button that opens the modal
  var btn = document.getElementById("myBtn");
-
- var button = document.getElementById("myBtn2");
-
+ var btn2 = document.getElementById("myBtn2");
+ var btn3 = document.getElementById("myBtn3");
+ var btn4 = document.getElementById("myBtn4");
+ var btn5 = document.getElementById("myBtn5");
+ var btn6 = document.getElementById("myBtn6");
+ var btn7 = document.getElementById("myBtn7");
+ var btn8 = document.getElementById("myBtn8");
 
  // Get the <span> element that closes the modal
  var span = document.getElementsByClassName("close")[0];
@@ -17,7 +21,31 @@ window.onload = function(){
      modal.style.display = "block";
     }
 
-    button.onclick = function() {
+    btn2.onclick = function() {
+        modal.style.display = "block";
+       }
+
+       btn3.onclick = function() {
+        modal.style.display = "block";
+       }
+
+       btn4.onclick = function() {
+        modal.style.display = "block";
+       }
+
+       btn5.onclick = function() {
+        modal.style.display = "block";
+       }
+
+       btn6.onclick = function() {
+        modal.style.display = "block";
+       }
+
+       btn7.onclick = function() {
+        modal.style.display = "block";
+       }
+
+       btn8.onclick = function() {
         modal.style.display = "block";
        }
 
