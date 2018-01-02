610645d6439d16c565a1a45bcc4ed328d04ae9b7
tolavolaoke <tolavolaoke@hotmail.co.uk>
2018-01-02 16:48:10 +0000

    fixed the modal for the second button

--- js/main.js
+++ js/main.js
@@ -6,6 +6,9 @@ window.onload = function(){
  // Get the button that opens the modal
  var btn = document.getElementById("myBtn");
 
+ var button = document.getElementById("myBtn2");
+
+
  // Get the <span> element that closes the modal
  var span = document.getElementsByClassName("close")[0];
  
@@ -13,6 +16,10 @@ window.onload = function(){
  btn.onclick = function() {
      modal.style.display = "block";
     }
+
+    button.onclick = function() {
+        modal.style.display = "block";
+       }
 
  // When the user clicks on <span> (x), close the modal
  span.onclick = function() {
