978aedca749133f3c6f94f164245b21b7ee4ba10
tolavolaoke <tolavolaoke@hotmail.co.uk>
2018-06-15 13:16:59 +0100

    added bootstrap and a template

--- js/main.js
+++ js/main.js
@@ -1,60 +1,198 @@
-window.onload = function(){ 
+;(function () {
+	
+	'use strict';
 
-    // Get the modal
- var modal = document.getElementById('myModal');
- var box = document.getElementById('myModal2');
-var simply = document.getElementById('myModal3');
-var d = document.getElementById('myModal4');
+	var mobileMenuOutsideClick = function() {
 
- // Get the button that opens the modal
- var btn = document.getElementById("myBtn");
- var btn2 = document.getElementById("myBtn2");
- var btn3 = document.getElementById("myBtn3");
- var btn4 = document.getElementById("myBtn4");
+		$(document).click(function (e) {
+	    var container = $("#fh5co-offcanvas, .js-fh5co-nav-toggle");
+	    if (!container.is(e.target) && container.has(e.target).length === 0) {
 
- // When the user clicks the button, open the modal 
- btn.onclick = function() {
-     modal.style.display = "block";
-    }
+	    	if ( $('body').hasClass('offcanvas') ) {
 
-    btn2.onclick = function() {
-        box.style.display = "block";
-       }
+    			$('body').removeClass('offcanvas');
+    			$('.js-fh5co-nav-toggle').removeClass('active');
+				
+	    	}
+	    
+	    	
+	    }
+		});
 
-       btn3.onclick = function() {
-        modal.style.display = "block";
-       }
-
-       btn4.onclick = function() {
-        modal.style.display = "block";
-       }
-
- // Get the <span> element that closes the modal
- var span = document.getElementsByClassName("close");
-
- // When the user clicks on <span> (x), close the modal
- for(var each in span){
-    span[each].onclick = function() {
-        var parentModal = this.parentElement.parentElement;
-        parentModal.style.display = "none";
-     }
- }
- 
- // When the user clicks anywhere outside of the modal, close it
- window.onclick = function(event) {
-     if (event.target == modal) {
-         modal.style.display = "none";
-     }
- }  
-
-//Show expanded image when the non expanded image had been clicked
-    document.getElementsByClassName("small-modal-image")[0].addEventListener('click', function (event) {
-        console.log("it works!!!");
-    document.getElementsByClassName("big-modal-image")[0].style.display = "block";
+	};
 
 
-});
+	var offcanvasMenu = function() {
+
+		$('#page').prepend('<div id="fh5co-offcanvas" />');
+		$('#page').prepend('<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle fh5co-nav-white"><i></i></a>');
+		var clone1 = $('.menu-1 > ul').clone();
+		$('#fh5co-offcanvas').append(clone1);
+		var clone2 = $('.menu-2 > ul').clone();
+		$('#fh5co-offcanvas').append(clone2);
+
+		$('#fh5co-offcanvas .has-dropdown').addClass('offcanvas-has-dropdown');
+		$('#fh5co-offcanvas')
+			.find('li')
+			.removeClass('has-dropdown');
+
+		// Hover dropdown menu on mobile
+		$('.offcanvas-has-dropdown').mouseenter(function(){
+			var $this = $(this);
+
+			$this
+				.addClass('active')
+				.find('ul')
+				.slideDown(500, 'easeOutExpo');				
+		}).mouseleave(function(){
+
+			var $this = $(this);
+			$this
+				.removeClass('active')
+				.find('ul')
+				.slideUp(500, 'easeOutExpo');				
+		});
 
 
- };
+		$(window).resize(function(){
 
+			if ( $('body').hasClass('offcanvas') ) {
+
+    			$('body').removeClass('offcanvas');
+    			$('.js-fh5co-nav-toggle').removeClass('active');
+				
+	    	}
+		});
+	};
+
+
+	var burgerMenu = function() {
+
+		$('body').on('click', '.js-fh5co-nav-toggle', function(event){
+			var $this = $(this);
+
+
+			if ( $('body').hasClass('overflow offcanvas') ) {
+				$('body').removeClass('overflow offcanvas');
+			} else {
+				$('body').addClass('overflow offcanvas');
+			}
+			$this.toggleClass('active');
+			event.preventDefault();
+
+		});
+	};
+
+
+
+	var contentWayPoint = function() {
+		var i = 0;
+		$('.animate-box').waypoint( function( direction ) {
+
+			if( direction === 'down' && !$(this.element).hasClass('animated-fast') ) {
+				
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function(){
+
+					$('body .animate-box.item-animate').each(function(k){
+						var el = $(this);
+						setTimeout( function () {
+							var effect = el.data('animate-effect');
+							if ( effect === 'fadeIn') {
+								el.addClass('fadeIn animated-fast');
+							} else if ( effect === 'fadeInLeft') {
+								el.addClass('fadeInLeft animated-fast');
+							} else if ( effect === 'fadeInRight') {
+								el.addClass('fadeInRight animated-fast');
+							} else {
+								el.addClass('fadeInUp animated-fast');
+							}
+
+							el.removeClass('item-animate');
+						},  k * 200, 'easeInOutExpo' );
+					});
+					
+				}, 100);
+				
+			}
+
+		} , { offset: '85%' } );
+	};
+
+
+	var dropdown = function() {
+
+		$('.has-dropdown').mouseenter(function(){
+
+			var $this = $(this);
+			$this
+				.find('.dropdown')
+				.css('display', 'block')
+				.addClass('animated-fast fadeInUpMenu');
+
+		}).mouseleave(function(){
+			var $this = $(this);
+
+			$this
+				.find('.dropdown')
+				.css('display', 'none')
+				.removeClass('animated-fast fadeInUpMenu');
+		});
+
+	};
+
+	var goToTop = function() {
+
+		$('.js-gotop').on('click', function(event){
+			
+			event.preventDefault();
+
+			$('html, body').animate({
+				scrollTop: $('html').offset().top
+			}, 500, 'easeInOutExpo');
+			
+			return false;
+		});
+
+		$(window).scroll(function(){
+
+			var $win = $(window);
+			if ($win.scrollTop() > 200) {
+				$('.js-top').addClass('active');
+			} else {
+				$('.js-top').removeClass('active');
+			}
+
+		});
+	
+	};
+
+
+	// Loading page
+	var loaderPage = function() {
+		$(".fh5co-loader").fadeOut("slow");
+	};
+
+	var counter = function() {
+		$('.js-counter').countTo({
+			 formatter: function (value, options) {
+	      return value.toFixed(options.decimals);
+	    },
+		});
+	};
+
+	
+	$(function(){
+		mobileMenuOutsideClick();
+		offcanvasMenu();
+		burgerMenu();
+		contentWayPoint();
+		dropdown();
+		goToTop();
+		loaderPage();
+	});
+
+
+}());
---
+++ jsold/main.js
@@ -0,0 +1,60 @@
+// window.onload = function(){ 
+
+//     // Get the modal
+//  var modal = document.getElementById('myModal');
+//  var box = document.getElementById('myModal2');
+// var simply = document.getElementById('myModal3');
+// var d = document.getElementById('myModal4');
+
+//  // Get the button that opens the modal
+//  var btn = document.getElementById("myBtn");
+//  var btn2 = document.getElementById("myBtn2");
+//  var btn3 = document.getElementById("myBtn3");
+//  var btn4 = document.getElementById("myBtn4");
+
+//  // When the user clicks the button, open the modal 
+//  btn.onclick = function() {
+//      modal.style.display = "block";
+//     }
+
+//     btn2.onclick = function() {
+//         box.style.display = "block";
+//        }
+
+//        btn3.onclick = function() {
+//         modal.style.display = "block";
+//        }
+
+//        btn4.onclick = function() {
+//         modal.style.display = "block";
+//        }
+
+//  // Get the <span> element that closes the modal
+//  var span = document.getElementsByClassName("close");
+
+//  // When the user clicks on <span> (x), close the modal
+//  for(var each in span){
+//     span[each].onclick = function() {
+//         var parentModal = this.parentElement.parentElement;
+//         parentModal.style.display = "none";
+//      }
+//  }
+ 
+//  // When the user clicks anywhere outside of the modal, close it
+//  window.onclick = function(event) {
+//      if (event.target == modal) {
+//          modal.style.display = "none";
+//      }
+//  }  
+
+// //Show expanded image when the non expanded image had been clicked
+//     document.getElementsByClassName("small-modal-image")[0].addEventListener('click', function (event) {
+//         console.log("it works!!!");
+//     document.getElementsByClassName("big-modal-image")[0].style.display = "block";
+
+
+// });
+
+
+//  };
+
